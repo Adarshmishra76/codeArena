5624a4cf336cfead95e758f506fb17ea4e501eef
Adarsh Mishra <118348557+Adarshmishra76@users.noreply.github.com>
2026-08-25 14:28:10 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: "https://codearena-r2du.onrender.com",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
